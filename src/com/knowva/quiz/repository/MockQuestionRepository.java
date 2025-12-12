d27472f79121bc2adbcd3db6db829b606bd86a66
Daniel-Lou-Mar <danilou113@gmail.com>
2025-12-12 17:44:47 +0000

    MockQuestionRepository creation (get possible questions)

--- src/com/knowva/quiz/repository/MockQuestionRepository.java
+++ src/com/knowva/quiz/repository/MockQuestionRepository.java
@@ -1,8 +1,5 @@
 package com.knowva.quiz.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MockQuestionRepository implements QuestionRepository{
     @Override
     public getQuestionsByTopic(Topic topic) {
